0cf536169f8b679e3a0affbd1457856dbe87e57f
Wadephul <christian.wadephul@gmail.com>
2022-06-07 19:16:51 +0200

    Saved plots as pdf #12 and cleaned code #15

--- train.py
+++ train.py
@@ -1,55 +1,16 @@
-# dataframe and plotting
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-# machine learning
-#from lightgbm import LGBMClassifier
 from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error as mse
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import confusion_matrix, classification_report
-#from google.colab import files
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix, ConfusionMatrixDisplay, classification_report
 import warnings
 warnings.filterwarnings('ignore')
-
-# function to preprocess our data from train models
-
-enc = LabelEncoder()
-
-def preprocessing_data(df):
-    # convert categorical features to numerical features
-    # # checking categorical columns
-    categorical_columns = df.select_dtypes(include = 'object')
-    categorical_columns
-
-    # categorical features to be converted by One Hot Encoding
-    categ =  ["relationship_with_head", "marital_status", "education_level", "job_type", "country"]
-    # One Hot Encoding conversion
-    df = pd.get_dummies(df, prefix_sep='_', columns = categ)
-
-    # Labelncoder conversion
-    # #for i in categ:
-    # df[i] = enc.fit_transform(df[i])
-    df['location_type'] = enc.fit_transform(df['location_type'])
-    df['cellphone_access'] = enc.fit_transform(df['cellphone_access'])
-    df['gender_of_respondent'] = enc.fit_transform(df['gender_of_respondent'])    
-    
-    # drop uniquid column
-    df = df.drop(["uniqueid"], axis=1)
-    
-    # scale our data into range of 0 and 1
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    df = scaler.fit_transform(df)
-    
-    return df                  
-
-# später auch test_end Set!
-
 
 # Load files into a pandas dataframe
 train = pd.read_csv('data/Train.csv')
@@ -67,20 +28,18 @@ print('missing values:', train.isnull().sum())
 
 plotfontsize = 10
 
-# Explore Target distribution 
-#sns.catplot(x="country", y="bank_account", kind="count",  data=train)
-
-## number of people with bank account per country
+# number of people with bank account and per country and location type
 f,axes=plt.subplots(1,3,figsize=(16,5))
-sns.countplot(x='bank_account',hue='country',data=train,palette='magma',ax=axes[0]).set_title('People with bank account per country',fontsize=(plotfontsize))
-sns.countplot(x='bank_account',data=train,ax=axes[1]).set_title('People with bank account',fontsize=(plotfontsize))
-sns.countplot(x='bank_account',hue='location_type', data=train,palette='magma', ax=axes[2]).set_title('People without [0] vs. with [1] bank account per location type',fontsize=(plotfontsize))
-plt.show()
+sns.countplot(x='bank_account',hue='country',data=train,palette='magma',ax=axes[0]).set_title('Bank account per country',fontsize=(plotfontsize))
+sns.countplot(x='bank_account',data=train,ax=axes[1]).set_title('People with and without bank account',fontsize=(plotfontsize))
+sns.countplot(x='bank_account',hue='location_type', data=train,palette='magma', ax=axes[2]).set_title('Bank account per location type',fontsize=(plotfontsize))
+plt.savefig("bank_accounts_per_country_and_location_type.pdf")
 
-## finding correlation 
+# Drop uniqueid column 
 data_corr=train.copy()
 data_corr.drop('uniqueid',inplace=True,axis=1)
 
+# Transform categorical features to numerical values
 ll=LabelEncoder()
 data_corr['country']=ll.fit_transform(data_corr['country'])
 data_corr['bank_account']=ll.fit_transform(data_corr['bank_account'])
@@ -90,12 +49,13 @@ data_corr['gender_of_respondent']=ll.fit_transform(data_corr['gender_of_responde
 data_corr['relationship_with_head'] = ll.fit_transform(data_corr['relationship_with_head'])
 data_corr['marital_status']=ll.fit_transform(data_corr['marital_status'])
 data_corr['education_level']=ll.fit_transform(data_corr['education_level'])
-#data_corr['job_type']=ll.fit_transform(data_corr['job_type'])
-### correlation
+data_corr['job_type']=ll.fit_transform(data_corr['job_type'])
+
+# Plot correlation of all features
 sns.set_style('dark')
 f,axes=plt.subplots(1,1,figsize=(15,6))
 sns.heatmap(data_corr.corr(),vmin=0,vmax=1,annot=True)
-plt.show()
+plt.savefig("heatmap_correlations.pdf")
 
 y = train.bank_account  # Define target
 X = train.drop('bank_account', axis=1)  # Define predictors 
@@ -114,8 +74,6 @@ X_train.columns
 
 X_train.head()
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
 for i in categorical_columns:
     enc = LabelEncoder()
     print(i)
@@ -146,41 +104,22 @@ lg_model.fit(X_train, y_train)
 y_pred_baseline = baseline_model(X_test)
 y_pred_lg = lg_model.predict(X_test)
 
-# print confusion matrix baseline-model
+# print confusion matrix of baseline-model
 cm_baseline = confusion_matrix(y_test, y_pred_baseline)
 sns.heatmap(cm_baseline, cmap="YlGnBu", annot=True, fmt='d');
+plt.savefig("confusion_matrix_baseline_model.pdf")
+
 # print confusion matrix lg-model
 cm_lg_model = confusion_matrix(y_test, y_pred_lg)
 sns.heatmap(cm_lg_model, cmap="YlGnBu", annot=True, fmt='d');
+plt.savefig("confusion_matrix_lg_model.pdf")
 
-# Print classification report for more information
+# Print classification report for more information of the lg_model
 print(classification_report(y_test, y_pred_baseline))
 print(classification_report(y_test, y_pred_lg))
 
-plt.show()
-
-#rescale X_train and X_test with MinMaxScaler
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-X_train_rescaled = scaler.fit_transform(X_train)
-X_test_rescaled = scaler.fit_transform(X_test)
-
-# Instantiate model and fit it on rescaled train data
-lg_model.fit(X_train_rescaled, y_train)
-
-# Make predictions for test set
-y_pred_lg_rescaled = lg_model.predict(X_test_rescaled)
-
-# print confusion matrix lg-model rescaled train data
-cm_lg_rescaled = confusion_matrix(y_test, y_pred_lg_rescaled)
-sns.heatmap(cm_lg_rescaled, cmap="YlGnBu", annot=True, fmt='d');
-
-# Print classification report for more information on lg_model with rescaled data
-print(classification_report(y_test, y_pred_lg_rescaled))
-
 # my model 1: RandomForestClassifier
 my_model_1 = RandomForestClassifier(n_estimators=100, random_state= 20, n_jobs = 2)
-# my_model_1.score()
 
 # Instantiate model and fit it on train data
 my_model_1.fit(X_train, y_train)
@@ -188,15 +127,15 @@ my_model_1.fit(X_train, y_train)
 # Make predictions for test set
 y_pred_my_model_1 = my_model_1.predict(X_test)
 
-# print confusion matrix my_model_1 on train data
+# print confusion matrix of Model 1: Random Forest on train data
 cm_my_model_1 = confusion_matrix(y_test, y_pred_my_model_1)
 sns.heatmap(cm_my_model_1, cmap="YlGnBu", annot=True, fmt='d');
+plt.savefig("confusion_matrix_model_1.pdf")
 
 # Print classification report for my_model_1 on test data
 print(classification_report(y_test, y_pred_my_model_1))
 
 # My Model 2: Optimize model paramaters via Grid Search
-# I run this code in google colab to make the execution much faster and use the best params in the next code
 param_grid = {#'class_weight': [2, 5, 10],
            # 'gamma': [0.5, 1, 1.5, 2, 5],
             'max_samples': [1, 10, 100],
@@ -214,9 +153,10 @@ print(my_model_2.best_params_)
 # Make predictions for test set
 y_pred_my_model_2 = my_model_2.predict(X_test)
 
-# print confusion matrix my_model_1 on train data
+# print confusion matrix of Model 2 as Optimized Model 1 with Grid Search
 cm_my_model_2 = confusion_matrix(y_test, y_pred_my_model_2)
 sns.heatmap(cm_my_model_2, cmap="YlGnBu", annot=True, fmt='d');
+plt.savefig("confusion_matrix_model_2.pdf")
 
 # Print classification report for my_model_2 on test data
 print(classification_report(y_test, y_pred_my_model_2))
@@ -227,5 +167,15 @@ print(classification_report(y_test, y_pred_my_model_1))
 # Print again classification report for lg_model
 print(classification_report(y_test, y_pred_lg))
 
-plt.show()
+# Print precision for the three models
+print("Precision of LogisticRegression: {}".format(precision_score(y_test, y_pred_lg)))
+print("Precision of Model 1: RandomForests: {}".format(precision_score(y_test, y_pred_my_model_1)))
+print("Precision of Model 2: Optimized Model 1: {}".format(precision_score(y_test, y_pred_my_model_2)))
+# classifiers = {
+#     "Baseline Model": baseline_model(),
+#     "Logistic Regression": lg_model(),
+#     "Model 1 = RandomForestClassifier": my_model_1(),
+#     "Model 2 = GridSearchCV of Model 1": my_model_2(),
+# }
 
+# f, axes = plt.subplots(1, 4, figsize=(20, 5), sharey='row')
